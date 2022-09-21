e0d571b8f8aa6d1a1476ccff5b83eaefb76324b6
emi7718 <emilioair@hotmail.com>
2022-09-21 15:00:12 -0300

    addd

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ import sqlite3
 conn = sqlite3.connect('customer.db')
 
 
-c = conn.cursor()
+cursor = conn.cursor()
 
-c.execute('''INSERT INTO customers VALUES ('John', 'Elder', 'john@codemy.com'
+cursor.execute('''INSERT INTO customers VALUES ('John', 'Elder', 'john@codemy.com'
 )''')
 print('command executed')
 conn.commit()
