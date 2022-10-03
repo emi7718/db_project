47a0ef450908e8b97d639dd40094c5f195a2f3f6
emi7718 <emilioair@hotmail.com>
2022-10-03 17:54:36 -0300

    afasa

--- dj_poll/mysite/polls/views.py
+++ dj_poll/mysite/polls/views.py
@@ -2,8 +2,9 @@
 # Create your views here.
 
 from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+
 from .models import Question
-from django.shortcuts import render
 
 
 def index(request):
@@ -12,14 +13,12 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404('Question does not exist')
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
-    return HttpResponse('You\'re looking at the results of question %s' % question_id)
+    return HttpResponse('You\'re looking at the results \
+        of question %s' % question_id)
 
 def vote(request, question_id):
     return HttpResponse('you\'re voting on question %s' % question_id)
