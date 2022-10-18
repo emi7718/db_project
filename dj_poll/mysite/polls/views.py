713d982d35de19a47b88780bfd6762bc425421fb
emi7718 <emilioair@hotmail.com>
2022-10-18 17:54:26 -0300

    fgsfdgsd

--- dj_poll/mysite/polls/views.py
+++ dj_poll/mysite/polls/views.py
@@ -1,25 +1,24 @@
 #from django.shortcuts import render  <<< this was here when I opened views.py for the 1st time
 # Create your views here.
 
-from django.http import HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404
-
-from .models import Question
+from django.http import HttpResponse
+from django.template import loader
+from polls.models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('pub_date')[:5]
-    context = {'latest_question_list':latest_question_list}   
-    return render(request, 'polls/index.html', context)
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question':question})
+    return HttpResponse('you\'re looking at question %s' % question_id)
 
 def results(request, question_id):
-    return HttpResponse('You\'re looking at the results \
-        of question %s' % question_id)
+    response = 'You\'re looking at the results of question %s'
+    return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse('you\'re voting on question %s' % question_id)
+    return HttpResponse('You\'re voting on question %s' % question_id)
 
